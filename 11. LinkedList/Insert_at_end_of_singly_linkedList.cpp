cc68b1e1bb7cb2a90ede7bbb8f5d003101043cab
Ravi <ravkchauhan1aip@gmail.com>
2023-01-03 23:51:21 +0530

    Update

---
+++ 11. LinkedList/Insert_at_end_of_singly_linkedList.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+struct Node {
+    int data;
+    Node *next;
+    Node (int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head) {
+    Node *curr = head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr = curr->next;
+    }
+}
+
+Node *insertEnd(Node *head, int x){
+    Node *temp = new Node(x);
+    if(head==NULL) {
+        return temp;
+    }
+    Node *curr = head;
+    while(curr->next!=NULL) {
+        curr = curr->next;
+    }
+    curr->next = temp;
+
+    return head;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    printlist(head);
+    cout<<endl;
+    head = insertEnd(head, 5);
+    
+    printlist(head);
+
+    return 0;
+}
