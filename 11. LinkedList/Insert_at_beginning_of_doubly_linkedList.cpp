bb08c1efb7c7573e0fc4be2a78018b5fce113f59
Ravi <ravkchauhan1aip@gmail.com>
2023-01-03 23:53:28 +0530

    Update

---
+++ 11. LinkedList/Insert_at_beginning_of_doubly_linkedList.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+    Node (int x) {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+
+void print(Node *head) {
+    while(head!=NULL)
+    {
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+}
+
+
+Node *insertBegin(Node *head, int x){
+    Node *temp = new Node(x);
+
+    temp->next = head;
+    if(head!=NULL)
+        head->prev = temp;
+
+    return temp;
+}
+
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+    head->next = temp1;
+    temp1->next = temp2;
+    temp1->prev = head;
+    temp2->prev = temp1;
+
+    print(head);
+
+    head = insertBegin(head, 100);  
+
+    cout<<endl;
+    print(head);
+
+    return 0;
+}
