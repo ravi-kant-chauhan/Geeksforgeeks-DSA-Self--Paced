bb23e6d39c727b7051a6832ade92de06a9e7ea6a
Ravi <ravkchauhan1aip@gmail.com>
2023-01-03 23:52:01 +0530

    Update

---
+++ 11. LinkedList/Insert_at_beginning_of_singly_linkedList.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+struct Node {
+    int data;
+    Node *next;
+    Node (int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head) {
+    Node *curr = head;
+    while(curr!=NULL){
+        cout<<curr->data<<" ";
+        curr = curr->next;
+    }
+}
+
+
+Node *insertBegin(Node *head, int x){
+    Node *temp = new Node(x);
+    temp->next = head;
+    return temp;
+}
+
+
+int main()
+{
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+    printlist(head);
+    cout<<endl;
+    head = insertBegin(head, 5);
+
+    printlist(head);
+
+    return 0;
+}
