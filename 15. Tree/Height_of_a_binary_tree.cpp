cdf19bfd8e14ce093d367107c73486580cd1b3fd
Ravi <ravkchauhan1aip@gmail.com>
2023-01-08 23:58:28 +0530

    update

---
+++ 15. Tree/Height_of_a_binary_tree.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node
+{
+    int key;
+    Node *left;
+    Node *right;
+    Node (int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+int height(Node *root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    else
+        return max(height(root->left), height(root->right))+1;
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    root->left = new Node(20);
+    root->right = new Node(30);
+
+    cout<<height(root);
+
+    return 0;
+}
